17a89f079b1e4d00abf3cfe6db74c7cc0d2102b3
Ben <56520085@qq.com>
2016-08-07 17:44:31 +0800

    UserConfig added

---
+++ Norway/Norway/Models/UserConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Norway.Models
+{
+    /// <summary>
+    /// 用户配置
+    /// <remarks>
+    /// Created at 2016.08.07
+    /// </remarks>
+    /// </summary>
+    public class UserConfig
+    {
+
+    }
+}
