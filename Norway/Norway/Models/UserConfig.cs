fd1425a998c38858835106651e3ebd9cc89ea7ab
Peng Gao <txq_417@163.com>
2016-08-08 17:04:07 +0800

    InterfaceBaseRepository added UserConfig finished

--- Norway/Norway/Models/UserConfig.cs
+++ Norway/Norway/Models/UserConfig.cs
@@ -14,6 +14,35 @@ namespace Norway.Models
     /// </summary>
     public class UserConfig
     {
+        [Key]
+        public int ConfigID { get; set; }
 
+        /// <summary>
+        /// 禁止使用的用户名
+        /// 用户名之间用“|”隔开
+        /// </summary>
+        [Display(Name = "禁止使用的用户名")]
+        public string ProhibitUserName { get; set; }
+
+        /// <summary>
+        /// 启用管理员验证
+        /// </summary>
+        [Display(Name = "启用管理员验证")]
+        [Required(ErrorMessage = "必填")]
+        public bool EnableAdminVerify { get; set; }
+
+        /// <summary>
+        /// 启用邮件验证
+        /// </summary>
+        [Display(Name = "启用邮件验证")]
+        [Required(ErrorMessage = "必填")]
+        public bool EnableEmailVerify { get; set; }
+
+        /// <summary>
+        /// 默认用户组Id
+        /// </summary>
+        [Display(Name = "默认用户组Id")]
+        [Required(ErrorMessage = "必填")]
+        public int DefaultGroupId { get; set; }
     }
 }
