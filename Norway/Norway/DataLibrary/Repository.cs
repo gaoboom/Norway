15e8d8af87d53603dc02fd2e19c6f69c7a6d2f48
Ben <56520085@qq.com>
2016-08-13 21:14:49 +0800

    pagelist added

--- Norway/Norway/DataLibrary/Repository.cs
+++ Norway/Norway/DataLibrary/Repository.cs
@@ -151,5 +151,98 @@ namespace Norway.DataLibrary
             return _list;
         }
         #endregion
+
+        //查找实体分页列表
+        #region FindPageList
+
+        /// <summary>
+        /// 查找分页列表
+        /// </summary>
+        /// <param name="pageSize">每页记录数。必须大于1</param>
+        /// <param name="pageIndex">页码。首页从1开始，页码必须大于1</param>
+        /// <param name="totalNumber">总记录数</param>
+        /// <returns></returns>
+        public IQueryable<T> FindPageList(int pageSize, int pageIndex, out int totalNumber)
+        {
+            OrderParam _orderParam = null;
+            return FindPageList(pageSize, pageIndex, out totalNumber, _orderParam);
+        }
+
+        /// <summary>
+        /// 查找分页列表
+        /// </summary>
+        /// <param name="pageSize">每页记录数。必须大于1</param>
+        /// <param name="pageIndex">页码。首页从1开始，页码必须大于1</param>
+        /// <param name="totalNumber">总记录数</param>
+        /// <param name="order">排序键</param>
+        /// <param name="asc">是否正序</param>
+        /// <returns></returns>
+        public IQueryable<T> FindPageList(int pageSize, int pageIndex, out int totalNumber, OrderParam orderParam)
+        {
+            return FindPageList(pageSize, pageIndex, out totalNumber, (T) => true, orderParam);
+        }
+
+        /// <summary>
+        /// 查找分页列表
+        /// </summary>
+        /// <param name="pageSize">每页记录数。必须大于1</param>
+        /// <param name="pageIndex">页码。首页从1开始，页码必须大于1</param>
+        /// <param name="totalNumber">总记录数</param>
+        /// <param name="where">查询表达式</param>
+        public IQueryable<T> FindPageList(int pageSize, int pageIndex, out int totalNumber, Expression<Func<T, bool>> where)
+        {
+            OrderParam _param = null;
+            return FindPageList(pageSize, pageIndex, out totalNumber, where, _param);
+        }
+
+        /// <summary>
+        /// 查找分页列表
+        /// </summary>
+        /// <param name="pageSize">每页记录数。</param>
+        /// <param name="pageIndex">页码。首页从1开始</param>
+        /// <param name="totalNumber">总记录数</param>
+        /// <param name="where">查询表达式</param>
+        /// <param name="orderParam">排序【null-不设置】</param>
+        /// <returns></returns>
+        public IQueryable<T> FindPageList(int pageSize, int pageIndex, out int totalNumber, Expression<Func<T, bool>> where, OrderParam orderParam)
+        {
+            OrderParam[] _orderParams = null;
+            if (orderParam != null) _orderParams = new OrderParam[] { orderParam };
+            return FindPageList(pageSize, pageIndex, out totalNumber, where, _orderParams);
+        }
+
+        /// <summary>
+        /// 查找分页列表
+        /// </summary>
+        /// <param name="pageSize">每页记录数。</param>
+        /// <param name="pageIndex">页码。首页从1开始</param>
+        /// <param name="totalNumber">总记录数</param>
+        /// <param name="where">查询表达式</param>
+        /// <param name="orderParams">排序【null-不设置】</param>
+        public IQueryable<T> FindPageList(int pageSize, int pageIndex, out int totalNumber, Expression<Func<T, bool>> where, OrderParam[] orderParams)
+        {
+            if (pageIndex < 1) pageIndex = 1;
+            if (pageSize < 1) pageSize = 10;
+            IQueryable<T> _list = DbContext.Set<T>().Where(where);
+            var _orderParames = Expression.Parameter(typeof(T), "o");
+            if (orderParams != null && orderParams.Length > 0)
+            {
+                for (int i = 0; i < orderParams.Length; i++)
+                {
+                    //根据属性名获取属性
+                    var _property = typeof(T).GetProperty(orderParams[i].PropertyName);
+                    //创建一个访问属性的表达式
+                    var _propertyAccess = Expression.MakeMemberAccess(_orderParames, _property);
+                    var _orderByExp = Expression.Lambda(_propertyAccess, _orderParames);
+                    string _orderName = orderParams[i].Method == OrderMethod.ASC ? "OrderBy" : "OrderByDescending";
+                    MethodCallExpression resultExp = Expression.Call(typeof(Queryable), _orderName, new Type[] { typeof(T), _property.PropertyType }, _list.Expression, Expression.Quote(_orderByExp));
+                    _list = _list.Provider.CreateQuery<T>(resultExp);
+                }
+            }
+            totalNumber = _list.Count();
+            return _list.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+        }
+
+        #endregion
     }
 }
