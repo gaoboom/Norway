599fcd990a8837cabf3fe19e4df6481084f54af6
Peng Gao <txq_417@163.com>
2016-08-31 16:48:00 +0800

    Admin batch delete function added

--- Norway/Norway/Areas/Control/Controllers/AdminController.cs
+++ Norway/Norway/Areas/Control/Controllers/AdminController.cs
@@ -124,5 +124,63 @@ namespace Norway.Areas.Control.Controllers
             }
             return Json(_res);
         }
+
+        /// <summary>
+        /// 删除 
+        /// Response.Code:1-成功，2-部分删除，0-失败
+        /// Response.Data:删除的数量
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public JsonResult DeleteJson(List<int> ids)
+        {
+            int _total = ids.Count();
+            Response _res = new Core.Types.Response();
+            int _currentAdminID = int.Parse(Session["AdminID"].ToString());
+            if (ids.Contains(_currentAdminID))
+            {
+                ids.Remove(_currentAdminID);
+            }
+            _res = adminManager.Delete(ids);
+            if (_res.Code == 1 && _res.Data < _total)
+            {
+                _res.Code = 2;
+                _res.Message = "共提交删除" + _total + "名管理员,实际删除" + _res.Data + "名管理员。\n原因：不能删除当前登录的账号";
+            }
+            else if (_res.Code == 2)
+            {
+                _res.Message = "共提交删除" + _total + "名管理员,实际删除" + _res.Data + "名管理员。";
+            }
+            return Json(_res);
+        }
+
+        /// <summary>
+        /// 删除 
+        /// Response.Code:1-成功，2-部分删除，0-失败
+        /// Response.Data:删除的数量
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public JsonResult DeleteJsonBatch(List<int> ids)
+        {
+            int _total = ids.Count();
+            Response _res = new Response();
+            int _currentAdminID = int.Parse(Session["AdminID"].ToString());
+            if (ids.Contains(_currentAdminID))
+            {
+                ids.Remove(_currentAdminID);
+            }
+            _res = adminManager.Delete(ids);
+            if (_res.Code == 1 && _res.Data < _total)
+            {
+                _res.Code = 2;
+                _res.Message = "共提交删除" + _total + "名管理员,实际删除" + _res.Data + "名管理员。\n原因：不能删除当前登录的账号";
+            }
+            else if (_res.Code == 2)
+            {
+                _res.Message = "共提交删除" + _total + "名管理员,实际删除" + _res.Data + "名管理员。";
+            }
+            return Json(_res);
+        }
     }
 }
