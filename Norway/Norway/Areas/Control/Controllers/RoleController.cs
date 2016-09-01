bb8dbcd000ee1221331cf8fdf3d0688289a9fbdc
Peng Gao <txq_417@163.com>
2016-09-01 17:49:53 +0800

    Prompt view added

---
+++ Norway/Norway/Areas/Control/Controllers/RoleController.cs
@@ -0,0 +1,23 @@
+﻿using Norway.Core;
+using Norway.Core.Users;
+using Norway.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Norway.Areas.Control.Controllers
+{
+    [AdminAuthorize]
+    public class RoleController : Controller
+    {
+        private RoleManager roleManager = new RoleManager();
+
+        // GET: Control/Role
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
